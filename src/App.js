a388ce98ec256bc48a5c8a6ea20a387da983cba5
Adrian Domin <domin._.adrian@wp.pl>
2022-07-02 12:25:07 +0200

    added say hi for my wife

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there :)
+          Hi Malinka ;) How are you today? ;) 
         </p>
         <a
           className="App-link"
